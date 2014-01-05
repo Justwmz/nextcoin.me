b42488e09c9b02030b41e2dfeef8b5d418e27fb4
Justwmz <justwmz509@gmail.com>
2014-01-05 18:40:12 +0200

    Добавлена функция для получения средств от пользователя и зачисления их на счёт

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$url="http://localhost:7874/nxt?requestType=getAccountTransactionIds&account=3642025151018909259&timestamp=0";
+$url="http://localhost:7874/nxt?requestType=getAccountTransactionIds&account=9159366087528595444&timestamp=0";
 $json = file_get_contents($url);
 $Item = json_decode($json,true);
 
@@ -7,11 +7,21 @@ foreach ($Item['transactionIds'] as $a) {
 	      $url2 = "http://localhost:7874/nxt?requestType=getTransaction&transaction=$a";
           $json2 = file_get_contents($url2);
           $obj = json_decode($json2);
-          echo "<br>".$obj->{'sender'};
+
+		  $link = mysql_connect('localhost','root','0rSo%232fzq12');
+          if (!$link) $loginerr .="Не удалось соединиться с БД";
+                mysql_select_db('nxt', $link);
+                $result = mysql_query("SELECT * FROM users WHERE id=1",$link);
+                while($row = mysql_fetch_assoc($result)) {
+
+          			if($obj->{'sender'} == $row['wallet_nxt'] ){
+          			echo "<br>".$obj->{'amount'};
+          			//echo "<br>".$obj->{'sender'};
+      	  			}
+ 		  }
           /*
          	Отключено до пояснения значения timestamp и его формулы.
           	echo "<br>".$obj->{'timestamp'};
           */
-          echo "<br>".$obj->{'amount'};
 }
 ?>

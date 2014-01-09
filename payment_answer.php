f736515db53deeda2fda0de248b5d1d9880c3085
Justwmz <justwmz509@gmail.com>
2014-01-09 11:08:54 +0200

    [Test] Правки в отправке плтежа 3

--- payment_answer.php
+++ payment_answer.php
@@ -1,8 +1,9 @@
 <?php
-$_POST['operation_xml'] = $operation_xml;
-$_POST['signature'] = $signature;
-
-$xml_decoded=base64_decode($xml);
-echo $operation_xml;
-echo $xml_decoded;
+//$xml_answer_orig = $_POST['operation_xml'];
+$xml_answer=base64_decode($_POST['operation_xml']);
+$signature_answer=base64_encode(sha1($merc_sig.$xml_answer.$merc_sig,1)); 
+$status = strpos($xml_answer, 'status');
+$amount = strpos($xml_answer, 'amount');
+echo $status;
+echo "<br>".$amount;
 ?>

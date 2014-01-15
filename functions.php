60ce460e82eaa726f5ad6073092fdd511cb9c78b
Justwmz <justwmz509@gmail.com>
2014-01-15 17:06:44 +0200

    Исправлена функция вычисления баланса пользователя. Внесены изменения в запрос вывода средств с NXT баланса

--- functions.php
+++ functions.php
@@ -6,7 +6,10 @@ function getBalance($user_id){
                 $result = mysql_query("SELECT * FROM users WHERE id=$user_id",$link);
                 while($row = mysql_fetch_assoc($result)){
                 	$sum = mysql_query("SELECT SUM(amount_pay) FROM payments WHERE sender = ".$row['wallet_nxt']."");
-                	$balance = mysql_result($sum, 0);
+                        $sum2 = mysql_query("SELECT SUM(amount) FROM withdraw WHERE user_id = $user_id");
+                	$all_pay = mysql_result($sum, 0);
+                        $all_withdraw = mysql_result($sum2, 0);
+                        $balance = $all_pay - $all_withdraw;
                 return $balance;
                 }
 }

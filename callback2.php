112236f7b62f65d4a7c348be36c4d74d300e57c8
Justwmz <justwmz509@gmail.com>
2014-02-12 14:27:22 +0200

    Правки вывода

--- callback2.php
+++ callback2.php
@@ -1,11 +1,13 @@
 <?php
 $transaction_hash = $_GET['transaction_hash'];
 $value_in_btc = $_GET['value'] / 100000000;
-$address = $_GET['address'];
-
+$address = $_GET['destination_address'];
+$my_address = "1LXMUa32niJXP2LCQ8dXG94u55CitDPucy";
 //Commented out to test, uncomment when live
-if ($_GET['test'] == true) {
-    return;
+
+if ($address == $my_address) {
+    echo "*ok*";
+  return;
 }
 
 echo $address . ' received a payment of ' . $value_in_btc . ' transaction hash ' . $transaction_hash;

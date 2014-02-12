449ba8597fe53b4ba460bf47e592c4c47883baa8
Justwmz <justwmz509@gmail.com>
2014-02-12 10:58:50 +0200

    Добавлена функция подсчёта баланса при выполненных операциях , необходимо длительное тестирование для проверки.

--- withdraw_nxt.php
+++ withdraw_nxt.php
@@ -45,19 +45,38 @@ $db = new SafeMySQL();
       <div class="container">
         <ul class="nav navbar-nav navbar-right">
             <?php
+                $oper = $db->getAll("SELECT * FROM history WHERE id = ?i",$user_id);
+                $summ_ordersell_nxt = 0;
+                $summ_ordersell_btc = 0;
+                $summ_orderbuy_nxt = 0;
+                $summ_orderbuy_btc = 0;
+                for($i = 0; $i<count($oper);$i++)
+                {
+                  if($oper[$i]['order_id'] == 1)
+                  {
+                    $summ_ordersell_nxt += $oper[$i]['amount_nxt'];
+                    $summ_ordersell_btc += $oper[$i]['amount_btc'];
+                  }
+                  if($oper[$i]['order_id'] == 2)
+                  {
+                    $summ_orderbuy_nxt += $oper[$i]['amount_nxt'];
+                    $summ_orderbuy_btc += $oper[$i]['amount_btc'];
+                  }    
+                }
                 $users = $db->getRow("SELECT * FROM users WHERE id=?i",$user_id);
                 $btc = $db->getRow("SELECT SUM(value) FROM btc_payments WHERE user_id = ?i",$user_id);
                 echo("<li><a href='profile.php?id=".$users['id']."'><span class='glyphicon glyphicon-usd'></span> <span class='label label-default'>");
-                echo $btc['SUM(value)'];
+                $balance_btc = ($btc['SUM(value)'] + $summ_ordersell_btc) - $summ_orderbuy_btc;
+                echo round($balance_btc,4);
                 echo("</span></a></li>");
 
                     $sum = $db->getRow("SELECT SUM(amount_pay) FROM payments WHERE sender = ".$users['wallet_nxt']."");
                     $sum2 = $db->getRow("SELECT SUM(amount) FROM withdraw WHERE user_id = ?i",$user_id);
                     $all_pay = $sum['SUM(amount_pay)'];
                     $all_withdraw = $sum2['SUM(amount)'];
-                    $balance = $all_pay - ($all_withdraw + $users['holdings']);
+                    $balance_nxt = $all_pay - ($all_withdraw + $users['holdings'] + $summ_orderbuy_nxt) + $summ_orderbuy_nxt;
                 echo("<li><a href='profile.php?id=".$users['id']."'><span class='glyphicon glyphicon-globe'></span> <span class='label label-default'>");
-                echo $balance;        
+                echo $balance_nxt;        
                 echo("</span></a></li>"); 
                 echo("<li><a href='profile.php?id=".$users['id']."'><span class='glyphicon glyphicon-lock'></span> <span class='label label-default'>");
                 echo $users['holdings'];        
